22669e24137a491bf498e90f5328e23bb13f23af
Jhepar <jhepar17@gmail.com>
2023-08-07 01:19:58 -0300

    agregue estilo mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 const menuItem = document.querySelectorAll (".menu-item");
+
+
+
+const menuBtn = document.querySelector("#menu-boton");
+
+menuBtn.addEventListener("click", function(){
+    document.body.classList.toggle("mobile-menu-active")
+});
+
+
+
 menuItem.forEach(function(Item){
     Item.addEventListener("click", function(e){
         const activeItem = document.querySelector (".active");
         activeItem.classList.remove("active");
         e.target.classList.add("active");
-   })
-   });
+});
+});
+
+/* Boton para camboar a modo oscuro */
+
+const cambioBtn = document.querySelector("#BtnModoDark");
+
+cambioBtn.addEventListener("click", function(){
+    document.body.classList.toggle("modo-dark");
+
+
+
+});
